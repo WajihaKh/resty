8b39a75197ec0f7f212202f3279a2695b31ee6a4
Wajiha Khan <jiya.r.kh@gmail.com>
2024-06-05 18:00:44 -0400

    phase 2 works with a JSONplaceholder

--- Components/Form/index.jsx
+++ Components/Form/index.jsx
@@ -2,43 +2,43 @@ import { useState } from 'react';
 import './Form.scss';
 
 const Form = ({ handleApiCall }) => {
-    const [url, seturl] = useState('');
-    const [method, setMethod] = useState('GET');
-    const [body, setBody] = useState('');
+  const [url, setUrl] = useState('https://swapi.dev/api/people/1');
+  const [method, setMethod] = useState('GET');
+  const [body, setBody] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formData = {
-            method,
-            url,
-            body: body ? JSON.parse(body) : undefined };
-            handleApiCall(formData);
-        };
-
-        return (
-            <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>URL: </span>
-                    <input name='url' type='text' value={url} onChange={(e) => seturl(e.target.value)} />
-                </label>
-                <label className="methods">
-                    <span className={method === 'GET' ? 'active' : ''} onClick={() => setMethod('GET')}>GET</span>
-                    <span className={method === 'POST' ? 'active' : ''} onClick={() => setMethod('POST')}>POST</span>
-                    <span className={method === 'PUT' ? 'active' : ''} onClick={() => setMethod('PUT')}>PUT</span>
-                    <span className={method === 'DELETE' ? 'active' : ''} onClick={() => setMethod('DELTE')}>DELETE</span>
-                </label>
-                {(method === 'POST' || method === 'PUT') && (
-                    <label>
-                        <span>Body: </span>
-                        <textarea name='body' value={body} onChange={(e) => setBody(e.target.value)} />
-                    </label>
-                )}
-                <button type='submit'>GO!</button>
-            </form>
-            </>
-        );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      method: method,
+      url: url,
+      body: body ? JSON.parse(body) : undefined
     };
+    handleApiCall(formData);
+  };
 
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>URL: </span>
+          <input name='url' type='text' value={url} onChange={(e) => setUrl(e.target.value)} />
+        </label>
+        <label className="methods">
+          <span className={method === 'GET' ? 'active' : ''} onClick={() => setMethod('GET')}>GET</span>
+          <span className={method === 'POST' ? 'active' : ''} onClick={() => setMethod('POST')}>POST</span>
+          <span className={method === 'PUT' ? 'active' : ''} onClick={() => setMethod('PUT')}>PUT</span>
+          <span className={method === 'DELETE' ? 'active' : ''} onClick={() => setMethod('DELETE')}>DELETE</span>
+        </label>
+        {(method === 'POST' || method === 'PUT') && (
+          <label>
+            <span>Body: </span>
+            <textarea name='body' value={body} onChange={(e) => setBody(e.target.value)} />
+          </label>
+        )}
+        <button type='submit'>GO!</button>
+      </form>
+    </>
+  );
+};
 
 export default Form;
